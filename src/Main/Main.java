58665df2e9bbe9e7b5b3bfd6816479402bea98d5
Marcos Rigel <marcosrigel99@gmail.com>
2023-05-31 12:48:30 -0300

    projeto finalizado

---
+++ src/Main/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+
+import java.io.FileReader;
+
+import Sintatico.AnalisadorSintatico;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		FileReader arquivo;
+		AnalisadorSintatico analisadorSintatico;
+		arquivo = new FileReader("Input");
+		
+		analisadorSintatico = new AnalisadorSintatico(arquivo);
+		analisadorSintatico.parser();		
+	}
+}
